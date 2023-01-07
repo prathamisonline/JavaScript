e16cca28cfae6b56a8bb94d5fe174ccf1003366b
Pratham <prathamsrivastavas@gmail.com>
2023-01-07 23:39:18 +0530

    added new codes

---
+++ ArrayDestructuring.js
@@ -0,0 +1,15 @@
+// Variable assignment.
+const foo = ["one", "two", "three"];
+
+const [one, two, three] = foo;
+console.log(one); // "one"
+console.log(two); // "two"
+console.log(three); // "three"
+
+// Swapping variables
+let a = 1;
+let b = 3;
+
+[a, b] = [b, a];
+console.log(a); // 3
+console.log(b); // 1
---
+++ Objectdestructuring.js
@@ -0,0 +1,6 @@
+// Variable assignment.
+const o = { p: 42, q: true };
+const { p, q } = o;
+
+console.log(p); // 42
+console.log(q); // true
